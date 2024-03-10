1947908d82b7791035de63564042ff3813dc5f52
emondesigner <152256898+emondesigner@users.noreply.github.com>
2024-03-10 22:37:45 +0600

    Add files via upload

---
+++ assets/js/active.js
@@ -0,0 +1,17 @@
+
+  $(function(){
+    $('.hero_area').slick({
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:5000,
+        dots:true,
+    });
+  })
+  // $(function(){
+  //   $('.product_card').slick({
+  //     autoplay:true,
+  //     autoplaySpeed:5000,
+  //     slidesToShow: 3,
+  //   });
+  // })
+  
